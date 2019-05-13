ffdf5774bc1266c4a802f2d85ed2b271a0e5bc98
mfrancis95 <mikefrancis95@gmail.com>
2019-05-13 10:20:18 -0400

    Fix issue where some servers wouldn't show up on the master server list and update sv_website

--- config/zandronum.sh
+++ config/zandronum.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/zandronum-server +exec /config/common.cfg +exec /config/$1 -file /data/$2 -iwad /iwads/DOOM2.WAD +sv_password $PASSWORD +sv_rconpassword $RCON_PASSWORD
+/bin/zandronum-server +exec /config/common.cfg +exec /config/$1 -file /data/$2 -iwad /iwads/DOOM2.WAD -port $3 +sv_password $PASSWORD +sv_rconpassword $RCON_PASSWORD
